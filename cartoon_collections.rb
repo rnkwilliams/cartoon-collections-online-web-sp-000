c8e7c800da5e58faad7145fd76784d6c44daea46
Ranika Williams <rnkprice07@gmail.com>
2019-05-13 11:37:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,10 +21,10 @@ end
   answer
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(contains_cheddar)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find do |cheese|
+  contains_cheddar.find do |cheese|
     cheese_types.include?(cheese)
   end
 end
