45396bd5f526763e1e3bcd2da12bfb2bce7e6a93
Ranika Williams <rnkprice07@gmail.com>
2019-05-13 11:30:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,17 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+  answer = false
+  long_planteer_calls.each do |call|
+  if call.length > 4
+    answer = true
+  end
+end
+  answer
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
